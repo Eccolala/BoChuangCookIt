8fd8d55bd5ebb81fa23e6d7793d572d972dac5f2
eccolala <764884989@qq.com>
2016-04-30 13:50:11 +0800

    MaterialViewPager and BoomButton Finished

---
+++ app/src/main/java/com/example/woops/cookit/fragment/RecyclerViewFragment.java
@@ -0,0 +1,56 @@
+package com.example.woops.cookit.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.woops.cookit.R;
+import com.github.florent37.materialviewpager.MaterialViewPagerHelper;
+import com.github.florent37.materialviewpager.adapter.RecyclerViewMaterialAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class RecyclerViewFragment extends Fragment{
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+
+    private static final int ITEM_COUNT = 100;
+
+    private List<Integer> mContentItems = new ArrayList<>();
+
+    public static RecyclerViewFragment newInstance() {
+        return new RecyclerViewFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_recyclerview, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        mAdapter = new RecyclerViewMaterialAdapter(new RecyclerViewAdapter(mContentItems));
+        mRecyclerView.setAdapter(mAdapter);
+
+
+        for (int i = 0; i < ITEM_COUNT; ++i)
+            mContentItems.add(i);
+        mAdapter.notifyDataSetChanged();
+
+
+        MaterialViewPagerHelper.registerRecyclerView(getActivity(), mRecyclerView, null);
+    }
+}
